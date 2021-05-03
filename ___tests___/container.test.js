5b3a1414edb86db071aace260a060600aac5bc58
Javier Gonzalez <gonzalezbodasjavier@yahoo.es>
2021-05-03 13:54:52 +0200

    Restore form test

--- ___tests___/container.test.js
+++ ___tests___/container.test.js
@@ -1,131 +1,101 @@
 import React from 'react'
 import Container from 'components/Container'
+import { FormJSONContextProvider } from 'context/formJsonContext'
+import { TextEditorJSONContextProvider } from 'context/textEditorJsonContext'
 import { JSON_FILE_OBJECT_DEFAULT } from 'context/DEFAULT_PKG_JSON'
 import { server } from './setupWorkerAPI'
 import { render, screen, waitFor, fireEvent } from '@testing-library/react'
-import 'node_modules/jest-extended'
+import 'jest-extended';
 
 describe('Container Test', () => {
-  beforeEach(() => render(<Container />))
-  beforeAll(() => server.listen())
-  afterEach(() => server.resetHandlers())
-  afterAll(() => server.close())
 
-  describe('When initial load', () => {
-    it('should have default context values in form and texteditor', () => {
-      Object.keys(JSON_FILE_OBJECT_DEFAULT).map(key => {
-        if (typeof JSON_FILE_OBJECT_DEFAULT[key] === 'string') {
-          const input = screen.queryByRole('form-' + key)
-          input ? expect(input.value).toBe(JSON_FILE_OBJECT_DEFAULT[key]) : null
-          expect(screen.getByRole('text-area-editor').value).toInclude(
-            JSON_FILE_OBJECT_DEFAULT[key]
-          )
-        }
-      })
-    })
-  })
-
-  describe('When modifying field form', () => {
-    const json = {
-      authorName: 'javi',
-      projectName: 'packagejson-generator',
-      version: '2.3.9',
-      description: 'Testing packagejson-generator',
-      mainFile: 'app.js'
+    const wrapper = ({ children }) => {
+        return (<TextEditorJSONContextProvider>
+                    <FormJSONContextProvider>
+                        {children}
+                    </FormJSONContextProvider>
+                </TextEditorJSONContextProvider>)
     }
 
-    it('should show changes in text editor (author)', () => {
-      fireEvent.change(screen.getByRole('form-author'), {
-        target: { value: json.authorName }
-      })
-      expect(screen.getByRole('text-area-editor').textContent).toInclude(
-        json.authorName
-      )
-    })
+    beforeEach(() => render(<Container />, {wrapper}) )
+    beforeAll(() => server.listen())
+    afterEach(() => server.resetHandlers() )
+    afterAll(() => server.close() )
 
-    it('should show changes in text editor (project name)', () => {
-      fireEvent.change(screen.getByRole('form-name'), {
-        target: { value: json.projectName }
-      })
-      expect(screen.getByRole('text-area-editor').textContent).toInclude(
-        json.projectName
-      )
-    })
-
-    it('should show changes in text editor (version)', () => {
-      fireEvent.change(screen.getByRole('form-version'), {
-        target: { value: json.version }
-      })
-      expect(screen.getByRole('text-area-editor').textContent).toInclude(
-        json.version
-      )
-    })
-
-    it('should show changes in text editor (description)', () => {
-      fireEvent.change(screen.getByRole('form-description'), {
-        target: { value: json.description }
-      })
-      expect(screen.getByRole('text-area-editor').textContent).toInclude(
-        json.description
-      )
-    })
-
-    it('should show changes in text editor (main)', () => {
-      fireEvent.change(screen.getByRole('form-main'), {
-        target: { value: json.mainFile }
-      })
-      expect(screen.getByRole('text-area-editor').textContent).toInclude(
-        json.mainFile
-      )
-    })
-  })
-
-  describe('When selecting package in dependencies combo ', () => {
-    const packageName = 'react'
-
-    beforeEach(async () => {
-      fireEvent.change(screen.getByRole('input-dependencies'), {
-        target: { value: packageName }
-      })
-
-      await waitFor(() => {
-        expect(screen.queryByRole('dependencies-list-item')).toBeDefined()
-        fireEvent.click(screen.getByRole('dependencies-list-item'), {
-          target: { innerText: packageName }
+    describe('When initial load', () => {
+        it('should have default context values in form and texteditor', () => {
+            Object.keys(JSON_FILE_OBJECT_DEFAULT).map((key) => {
+                if(typeof JSON_FILE_OBJECT_DEFAULT[key] === 'string'){
+                    const input = screen.queryByTestId('form-' + key)
+                    input ? expect(input.value).toBe(JSON_FILE_OBJECT_DEFAULT[key]) : null
+                    expect(screen.getByTestId('text-area-editor').value).toInclude(JSON_FILE_OBJECT_DEFAULT[key])
+                }
+            })
+            
         })
-      })
     })
 
-    it('should add package to dependencies list and text editor content', () => {
-      expect(screen.getByRole('dependencies-list').textContent).toInclude(
-        packageName
-      )
-      expect(screen.getByRole('text-area-editor').textContent).toInclude(
-        packageName
-      )
+    describe('When modifying field form', () => {
+        const json = { authorName : 'javi', projectName : 'packagejson-generator', version : '2.3.9', description : 'Testing packagejson-generator', mainFile : 'app.js' }
+
+        it('should show changes in text editor (author)', () => {
+            fireEvent.change(screen.getByTestId('form-author'), {target: { value: json.authorName }})
+            expect(screen.getByTestId('text-area-editor').textContent).toInclude(json.authorName)
+        })
+
+        it('should show changes in text editor (project name)', () => {
+            fireEvent.change(screen.getByTestId('form-name'), {target: { value: json.projectName }})
+            expect(screen.getByTestId('text-area-editor').textContent).toInclude(json.projectName)
+        })
+
+        it('should show changes in text editor (version)', () => {
+            fireEvent.change(screen.getByTestId('form-version'), {target: { value: json.version }})
+            expect(screen.getByTestId('text-area-editor').textContent).toInclude(json.version)
+        })
+
+        it('should show changes in text editor (description)', () => {
+            fireEvent.change(screen.getByTestId('form-description'), {target: { value: json.description }})
+            expect(screen.getByTestId('text-area-editor').textContent).toInclude(json.description)
+        })
+
+        it('should show changes in text editor (main)', () => {
+            fireEvent.change(screen.getByTestId('form-main'), {target: { value: json.mainFile }})
+            expect(screen.getByTestId('text-area-editor').textContent).toInclude(json.mainFile)
+        })
     })
-  })
 
-  describe('When adding script in form', () => {
-    const scriptKey = 'test-jest'
-    const scriptvalue = 'jest'
+    describe('When selecting package in dependencies combo ', () => {
+        const packageName = 'react'
+        
+        beforeEach(async () => {
+            fireEvent.change(screen.getByTestId('input-dependencies'), {target: { value: packageName }})
 
-    it('should add script to scripts list and text editor content', () => {
-      screen.getByRole('script-key').value = scriptKey
-      screen.getByRole('script-value').value = scriptvalue
+            await waitFor(() => {
+                expect(screen.queryByTestId('dependencies-list-item')).toBeDefined()
+                fireEvent.click(screen.getByTestId('dependencies-list-item'), {target : { innerText: packageName}})
+            })
+        })
 
-      fireEvent.click(screen.getByRole('script-add-btn'))
-      expect(screen.getByRole('scripts-list').textContent).toInclude(scriptKey)
-      expect(screen.getByRole('scripts-list').textContent).toInclude(
-        scriptvalue
-      )
-
-      expect(screen.getByRole('text-area-editor').textContent).toInclude(
-        scriptKey
-      )
-      expect(screen.getByRole('text-area-editor').textContent).toInclude(
-        scriptvalue
-      )
+        it('should add package to dependencies list and text editor content', () => {
+            expect(screen.getByTestId('dependencies-list').textContent).toInclude(packageName)
+            expect(screen.getByTestId('text-area-editor').textContent).toInclude(packageName)
+        })
     })
-  })
+
+    describe('When adding script in form', () => {
+        const scriptKey = 'test-jest'
+        const scriptvalue = 'jest'
+
+        it('should add script to scripts list and text editor content', () => {
+            screen.getByTestId('script-key').value = scriptKey
+            screen.getByTestId('script-value').value = scriptvalue
+
+            fireEvent.click(screen.getByTestId('script-add-btn'))
+            expect(screen.getByTestId('scripts-list').textContent).toInclude(scriptKey)
+            expect(screen.getByTestId('scripts-list').textContent).toInclude(scriptvalue)
+
+            expect(screen.getByTestId('text-area-editor').textContent).toInclude(scriptKey)
+            expect(screen.getByTestId('text-area-editor').textContent).toInclude(scriptvalue)
+        })
+    })
 })
