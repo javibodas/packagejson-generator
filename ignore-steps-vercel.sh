4998a35a44ad82e0cc3ac93f025af8e417bc7e04
Javier Gonzalez Bodas <gonzalezbodasjavier@yahoo.es>
2022-10-19 20:36:43 +0200

    fix: fix breaklines encoding file

--- ignore-steps-vercel.sh
+++ ignore-steps-vercel.sh
@@ -3,10 +3,9 @@
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
-  echo "✅ - Build can proceed"
+  echo "- Build can proceed"
   exit 1;
-
 else
-  echo "🛑 - Build cancelled"
+  echo "- Build cancelled"
   exit 0;
 fi
