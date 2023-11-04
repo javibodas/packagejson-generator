4f47d1d5123f07210a348cce81fa071ae3bdd3c5
Javier Gonzalez Bodas <gonzalezbodasjavier@yahoo.es>
2023-11-04 20:26:43 +0100

    refactor: rename user id in tojson after/before getting/setting database

--- src/server/models/User.js
+++ src/server/models/User.js
@@ -3,7 +3,16 @@ import { Schema, model, models } from 'mongoose'
 
 const UserSchema = new Schema({
 	_id: { type: String }
-}, { timestamps: true, versionKey: false })
+}, { 
+	timestamps: true, 
+	versionKey: false,
+	toJSON: {
+		transform(doc, ret) {
+			ret.id = ret._id
+			delete ret._id
+		}
+	}
+})
 
 
 const UserModel = models.User || model('User', UserSchema)
